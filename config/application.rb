79399d6a1fe50068ef51f84dc27e0a22c3d6d385
syedali90 <syedali.90@gmail.com>
2013-05-29 23:43:20 +0500

    dont init on precompile

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,7 @@ module OneBS
     config.assets.enabled                              = true
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
     config.assets.paths << "#{Rails.root}/app/assets/PIE"
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
