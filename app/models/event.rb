729f318c07178182c98fe31534ab6c0e4259f610
syed ali <syedali.90@gmail.com>
2013-07-31 20:59:57 +0500

    caledar fixes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  attr_accessible :description, :location, :name, :start_time, :time, :website, :is_approved
+  attr_accessible :description, :location, :name, :start_time, :time, :website, :is_approved, :zip
 
 
   default_scope where(is_approved: true)
