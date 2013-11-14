4762106330d171bb56f39e2dcbfd07fe46d0fdc1
Nikita Gnotov <n.gnotov@gmail.com>
2013-11-14 18:18:15 +0200

    optimization

--- app/models/client_spotlight_image.rb
+++ app/models/client_spotlight_image.rb
@@ -3,7 +3,7 @@ class ClientSpotlightImage < ActiveRecord::Base
   attr_accessible :asset
   belongs_to :client_spotlight
 
-  has_attached_file :asset, :styles => {:large => "500x400#", :slider => "200x200#", :medium => "250x250#", :thumb => "150x150#"}
+  has_attached_file :asset, :styles => {:large => "500x400#", :slider => "200x200#", :medium => "250x250#", :thumb => "150x150#"}, :convert_options => {:thumb => '-strip -interlace plane -quality 90', :medium => '-strip -interlace plane -quality 90', :large => '-strip -interlace plane -quality 90'}
 
 
 end
