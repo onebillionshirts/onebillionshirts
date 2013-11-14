6778dd98a3ba47481983dafef662906ee90a0ee2
Nikita Gnotov <n.gnotov@gmail.com>
2013-11-14 19:16:55 +0200

    optimization

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,8 +3,8 @@ class Customer < ActiveRecord::Base
 
   belongs_to :charity
 
-  has_attached_file :charity_logo, :styles => { :medium => "250x250>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  has_attached_file :logo, :styles => { :medium => "250x250>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :charity_logo, :styles => { :medium => "250x250>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", :convert_options => {:thumb => '-strip -interlace plane -quality 90', :medium => '-strip -interlace plane -quality 90'}
+  has_attached_file :logo, :styles => { :medium => "250x250>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", :convert_options => {:thumb => '-strip -interlace plane -quality 90', :medium => '-strip -interlace plane -quality 90'}
 
 
 end
