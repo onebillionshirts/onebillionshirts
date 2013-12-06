66ab6b3c5f9b2364f0406875ae6a3c2320172f8f
Nikita Gnotov <n.gnotov@gmail.com>
2013-12-06 03:08:49 +0200

    add badges page

--- public/badges.js
+++ public/badges.js
@@ -3,4 +3,4 @@ query = params.classList;
 var image_size = params.getAttribute("data-image");
 var charity_link = params.getAttribute("data-url");
 
-document.write('<a href="'+ charity_link +'" style="width: 220px; display: inline-block;"><img alt="" src="http://onebsdev.herokuapp.com/badges/'+ image_size +'.png"></a>');
+document.write('<a href="'+ charity_link +'" style="width: 220px; display: inline-block;"><img alt="" src="http://onebillionshirts.org/badges/'+ image_size +'.png"></a>');
